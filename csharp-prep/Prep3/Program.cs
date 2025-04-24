6a01db43f7b7ebda3f70478ee750d72b0fc6f889
nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>
2025-04-24 11:14:54 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool play = true;
+        int guess;
+        int guessCount;
+        int answer;
+        
+        while(play) {
+            guess = 0;
+            guessCount = 0;
+            Console.Write("What is the magic number? ");
+            answer = int.Parse(Console.ReadLine());
+            while (guess != answer) {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+                if(guess != answer) {
+                    string response = answer > guess ? "Higher" : "Lower";
+                    System.Console.WriteLine(response);
+                }
+            }
+            string pluralality = guessCount > 1? "guesses" : "guess";
+            System.Console.WriteLine($"\nCongratulations! It took you {guessCount} {pluralality}.");
+            System.Console.Write("Do you want to play again?(y/n) ");
+            play = Console.ReadLine().Trim().ToLower() == "y";
+            System.Console.WriteLine("");
+        }
+        System.Console.WriteLine("Thanks for playing!");
     }
 }
