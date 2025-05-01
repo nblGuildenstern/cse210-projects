9e6c4e24fe759bf5f797cda66bedd4b818ec610b
nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>
2025-05-01 11:11:02 -0600

    Learning 02

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,12 @@
+public class Resume {
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display() {
+        System.Console.WriteLine($"Name: {_name}");
+        System.Console.WriteLine("Jobs: ");
+        foreach(Job job in _jobs) {
+            job.Display();
+        }
+    }
+}
