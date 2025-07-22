9e840a85d162b928a7db88a8e0be12f3188e3cbb
nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>
2025-07-21 22:07:26 -0600

    Completed Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Blue", 4);
+        Console.WriteLine($"Square: {square.GetColor()}, {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Red", 4, 5);
+        Console.WriteLine($"Rectangle: {rectangle.GetColor()}, {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Yellow", 5);
+        Console.WriteLine($"Circle: {circle.GetColor()}, {circle.GetArea():0.00}");
     }
 }
